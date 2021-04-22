4ecff86b2ad4af653b4d4986f0a1bd41aeef64c8
blu-men <cjbb5eyv@icloud.com>
2021-04-22 23:18:47 +0900

    learn_controller

---
+++ customized_todo_app_with_DB/src/Controller/HelloController.php
@@ -0,0 +1,12 @@
+<?php
+namespace App\Controller;
+
+class HelloController extends AppController {
+
+	public $name = 'Hello';
+	public $autoRender = false;
+  	
+	public function index(){
+		echo "hello world!";
+	}
+}
