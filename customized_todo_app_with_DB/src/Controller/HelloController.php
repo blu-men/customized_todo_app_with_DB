f3a7e01c7e8ae21fab2969ceef4d9ef5b948f0f8
blu-men <cjbb5eyv@icloud.com>
2021-04-23 13:13:01 +0900

    learn_layout

--- customized_todo_app_with_DB/src/Controller/HelloController.php
+++ customized_todo_app_with_DB/src/Controller/HelloController.php
@@ -3,11 +3,16 @@ namespace App\Controller;
 
 class HelloController extends AppController {
 
-	public $name = 'Hello';
-	public $autoRender = true; 
-	
-	public function index(){
-		$this->viewBuilder()->autoLayout(false);
+	public function initialize(){
+		$this->name = 'Hello';
+		$this->viewBuilder()->autoLayout(true);		
+		$this->viewBuilder()->layout('Hello');		
 	}
-	
+
+	public function index(){
+		$this->set('msg','ヘッダーエレメント!!');
+		$n = rand(1,2);
+		$this->set('footer','Hello\footer' . $n);
+	}
+		
 }
